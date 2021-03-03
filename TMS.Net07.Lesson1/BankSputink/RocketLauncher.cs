903d08e048300c1553d51d14bb10c4d53498620c
JinOptimist <88jin@mail.ru>
2021-03-03 21:49:52 +0300

    Init commit
    Add base class and UT

---
+++ TMS.Net07.Lesson1/BankSputink/RocketLauncher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BankSputink.Rocket;
+using System.Linq;
+
+namespace BankSputink
+{
+    public class RocketLauncher
+    {
+        public RocketLauncher(List<ISputnik> sputniks)
+        {
+            Sputniks = sputniks;
+        }
+
+        public List<ISputnik> Sputniks { get; private set; }
+
+        public bool IsAnySputnikInProcess()
+        {
+            return Sputniks
+                .Any(x => x.IsReadyToLaunch);
+        }
+    }
+}
---
+++ TMS.Net07.Lesson1/BankSputink/Roket/ISputnik.cs
@@ -0,0 +1,11 @@
+﻿namespace BankSputink.Rocket
+{
+    public interface ISputnik
+    {
+        bool IsReadyToLaunch { get; set; }
+        int Mass { get; }
+        string Name { get; }
+
+        string Launch();
+    }
+}
