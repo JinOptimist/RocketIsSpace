785b83663e39b62d355cda8b2be3c9754e0652e6
Yana <77960186+yanakrupets@users.noreply.github.com>
2021-03-07 23:35:03 +0300

    ComfortStructure

---
+++ TMS.Net07.Lesson1/Rocket/ComfortStructure/CommandCenter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket.ComfortStructure
+{
+    class CommandCenter : IComfortStructure
+    {
+        public CommandCenter(double weight)
+        {
+            if (weight > 0)
+            {
+                Weight = weight;
+            }
+            else
+            {
+                throw new Exception("Wrong weight. Expected: weight > 0");
+            }
+        }
+        public double Weight { get; }
+
+        public List<string> Ledger;
+        public void GetSignal(string signal)
+        {
+            Ledger.Add(signal);
+        }
+        public void SendSignal()
+        {
+            Console.WriteLine("Enter your message: ");
+            Ledger.Add(Console.ReadLine());
+        }
+
+        public string GetInfo()
+        {
+            return $"Command center weight: {Weight}";
+        }
+    }
+}
---
+++ TMS.Net07.Lesson1/Rocket/ComfortStructure/Kitchen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket.ComfortStructure
+{
+    class Kitchen : IComfortStructure
+    {
+        private int _storageCapacity;
+        private int _seatsNumber;
+        public Kitchen(double weight, int storageCapacity, int seatsNumber)
+        {
+            if (weight > 0 &&
+                storageCapacity > 0 && storageCapacity < 20 &&
+                seatsNumber > 0 && seatsNumber < 10)
+            {
+                Weight = weight;
+                _storageCapacity = storageCapacity;
+                _seatsNumber = seatsNumber;
+            }
+            else
+            {
+                throw new Exception("Wrong data. Expected: " +
+                    "weight > 0, " +
+                    "0 < storage capacity < 20, " +
+                    "0 < seats number < 10");
+            }
+        }
+        public double Weight { get; }
+
+        public string GetInfo()
+        {
+            return $"Kitchen weight: {Weight}\n" +
+                $"Storage capacity: {_storageCapacity}\n" +
+                $"Seats number: {_seatsNumber}";
+        }
+    }
+}
