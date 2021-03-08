fced00a41ec5af3bb3d704d3c368d88b7d9a2ae9
sined07 <sined07@yandex.ru>
2021-03-09 01:06:58 +0300

    feat: add client variant

---
+++ TMS.Net07.Lesson1/HumansResources/Humans/Clients/ClientVariant.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HumansResources.Humans.Persons;
+using HumansResources.Humans.Orders;
+
+namespace HumansResources.Humans.Clients
+{
+    class ClientVariant : IContact
+
+    {
+       
+        //здесь мы реализовываем интерфейс IContact
+        
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public PhoneNumber PhoneNumber { get; set; }
+        public PostAddress PostAddress { get; set; }
+
+        //здесь мы будем хранить заказы этого клиента
+        private readonly List<Order> listOrders = new List<Order>();
+
+       //Оставь пустой конструктор для других
+        public ClientVariant() { 
+        }
+
+
+        /*Через конструктор получаем нужные поля, я умышленно пропускаю  PostAddress postAddress.
+        Мы можем не знать его или получить через setter потом. Вообще через конструктор, как правило,
+        передают минимальные параметры для работоспособности класса или его целесообразности.*/
+        public ClientVariant(string name, string email, PhoneNumber phoneNumber) {
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        //Можем сделать такой конструктор, когда сразу передаем order
+        public ClientVariant(string name,  Order order)
+        {
+            Name = name;           
+            listOrders.Add(order);
+        }
+
+        
+        //Далее придумываем какие-нибудь методы     
+        
+        public void SetOrder(Order order) {
+            listOrders.Add(order);
+        }
+
+        public List<Order> GetOrders()
+        {
+            return listOrders;
+        }
+
+        public int GetCountOrders() {
+            return listOrders.Count;
+        }
+
+        /*Осталось придумать какой-нибудь метод, лучше два, которые целесообразно
+        "покрыть" unit-тестами. Тут нужно проявить фантазию. У меня пока закончилось 
+        на сегодня, может завтра что-нибудь еще подскажу. */
+    }
+}
