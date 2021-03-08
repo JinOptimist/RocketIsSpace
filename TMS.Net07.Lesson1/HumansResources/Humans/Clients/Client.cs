c4c88fa89df5278c9ef1120d20e495d396042a2c
Anton Martinovich <iighostpwnzii@gmail.com>
2021-03-09 00:39:20 +0300

    client

--- TMS.Net07.Lesson1/HumansResources/Humans/Clients/Client.cs
+++ TMS.Net07.Lesson1/HumansResources/Humans/Clients/Client.cs
@@ -1,7 +1,47 @@
 ﻿
+using System;
 namespace HumansResources.Humans.Clients
 {
-    public class Client
+    class Client
     {
+        public int DataTime { get; set; }
+        public bool IsMale { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Lastname { get; set; }
+
+        public Client(string firstName, int age, string middlename, bool ismale, string lastName)
+        {
+            DataTime = age;
+            Name = firstName;
+            Surname = middlename;
+            IsMale = ismale;
+            Lastname = lastName;
+
+        }
+
+
+        public Client() { }
+
+        public string GetFullName()
+        {
+            return string.Format("{0} {1} {2}", Name, Surname, Lastname);
+        }
+
+        public static int GetAge(DateTime reference, DateTime birthday)
+        {
+            int age = reference.Year - birthday.Year;
+            if (reference < birthday.AddYears(age)) age--;
+
+            return age;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+            }
+        }
     }
 }
