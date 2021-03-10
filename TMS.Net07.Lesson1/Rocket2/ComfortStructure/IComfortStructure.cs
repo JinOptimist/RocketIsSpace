c5210c62fb4f1cd3045e7952287d08b055337a79
Vlad <vlad.eroshenko.96@mail.ru>
2021-03-10 19:10:12 +0300

    Rebase project to core3

---
+++ TMS.Net07.Lesson1/Rocket2/ComfortStructure/IComfortStructure.cs
@@ -0,0 +1,8 @@
+﻿namespace Rocket2.ComfortStructure
+{
+    public interface IComfortStructure : IRocket
+    {
+        double Mass { get; }
+        string GetInfo();
+    }
+}
---
+++ TMS.Net07.Lesson1/Rocket2/RocketFactory/MechanicFactory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Rocket2.Mechanics;
+
+namespace Rocket2.RocketFactory
+{
+    public class MechanicFactory : Factory
+    {
+        public override IRocket Create(int n)
+        {
+            var enginesList = EnginesFactory.Create(3);
+            var fuelTanksList = FuelTanksFactory.Create(10);
+            return new RocketStage(enginesList, fuelTanksList);
+        }
+    }
+
+    public class EnginesFactory
+    {
+        public static List<Engine> Create(int n)
+        {
+            var list = new List<Engine>();
+            for (var i = 0; i < n; i++)
+            {
+                list.Add(new Engine(40, 1500, 900));
+            }
+
+            return list;
+        }
+    }
+
+    public class FuelTanksFactory
+    {
+        public static List<FuelTank> Create(int n)
+        {
+            var list = new List<FuelTank>();
+            for (var i = 0; i < n; i++)
+            {
+                list.Add(new FuelTank(40, 1500));
+            }
+
+            return list;
+        }
+    }
+}
