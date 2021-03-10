16fc767bc5a382710ebc991758ec8d2e562ad5fc
sined07 <sined07@yandex.ru>
2021-03-10 11:49:56 +0300

    refactor: orders

--- TMS.Net07.Lesson1/HumansResources/Program.cs
+++ TMS.Net07.Lesson1/HumansResources/Program.cs
@@ -1,5 +1,4 @@
-﻿using HumansResources.Humans.EmployesVariant;
-using System;
+﻿using System;
 
 namespace HumansResources
 {
@@ -7,24 +6,7 @@ namespace HumansResources
     {
         static void Main(string[] args)
         {
-            Test();
             Console.WriteLine("Hello World!");
-        }
-
-        static void Test() 
-        {
-            Department department = new Department(Department.Type.Laboratory, 10);
-            DateTime.TryParse("2021-02-01 10:00", out DateTime dateStart);
-            DateTime.TryParse("2021-02-28", out DateTime dateEnd);
-            var result = department.GetCountWorkingHours(dateStart, dateEnd);
-            Console.WriteLine("countWorkingHour = " + result);            
-            department.SetEmploye(new Employe(null, Employe.Specification.Technicist, 1));
-            department.SetEmploye(new Employe(null, Employe.Specification.Technicist, 10));
-            department.SetEmploye(new Employe(null, Employe.Specification.Technicist, 10));
-            var countEmployes = department.GetCountEmployes(Employe.Specification.Technicist);
-            Console.WriteLine("countEmployes = " + countEmployes);
-            decimal cost = department.GetCostWorkingDepartment(dateStart, dateEnd);
-            Console.WriteLine("cost = " + cost);
         }
     }
 }
