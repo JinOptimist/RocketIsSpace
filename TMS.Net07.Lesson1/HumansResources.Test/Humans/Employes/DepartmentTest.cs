d1f26a7b383150ae6670114869411f0b8826c37c
KiriIl <375447360162@mail.ru>
2021-03-12 11:13:10 +0300

    finished DepartmentTest

--- TMS.Net07.Lesson1/HumansResources.Test/Humans/Employes/DepartmentTest.cs
+++ TMS.Net07.Lesson1/HumansResources.Test/Humans/Employes/DepartmentTest.cs
@@ -9,16 +9,28 @@ namespace HumansResources.Test.Humans.Employes
     class DepartmentTest
     {
         [Test]
-        [TestCase("2021-02-01", "2021-02-28", 22400)]
+        [TestCase("2021-02-01", "2021-02-28", 20000)]
+        [TestCase("2021-02-01", "2021-02-14", 10000)]
+        [TestCase("2021-01-01", "2021-01-31", 21000)]
+        [TestCase("2021-01-01", "2021-01-15", 10000)]
         public void GetCostWorkingDepartment(string dateStartStr, string DateEndStr, decimal expecredResult)
         {
             List<IEmploye> list = new List<IEmploye>();
+
             var employe1 = new Mock<IEmploye>();
-            employe1.Setup(x => x.SalaryPerHour).Returns(140);
+            employe1.Setup(x => x.SalaryPerHour).Returns(65);
             employe1.Setup(x => x.SpecificationType).Returns(Specification.Scientist);
+
+            var employe2 = new Mock<IEmploye>();
+            employe2.Setup(x => x.SalaryPerHour).Returns(60);
+            employe2.Setup(x => x.SpecificationType).Returns(Specification.Scientist);
+
             list.Add(employe1.Object);
+            list.Add(employe2.Object);
+
             Department department = new Department(Department.DepartmentType.Laboratory, "Laboratory#1", 2);
-            department.SetEmploye(list[0], out bool result);
+            foreach(var x in list)
+                department.SetEmploye(x, out bool result);
             DateTime.TryParse(dateStartStr, out DateTime dateStart);
             DateTime.TryParse(DateEndStr, out DateTime dateEnd);
             Assert.AreEqual(expecredResult, department.GetCostWorkingDepartment(dateStart, dateEnd));
