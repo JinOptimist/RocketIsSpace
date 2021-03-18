3b768929c6df76fa69805aef4e811dc61018e031
Vlad <vlad.eroshenko.96@mail.ru>
2021-03-18 21:57:42 +0300

    settings

--- TMS.Net07.Lesson1/VladHTML/js/index.js
+++ TMS.Net07.Lesson1/VladHTML/js/index.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
 
-    $('.central-block .rocket').onmouseover(function(){
-        $('.text').hide();
-    })
+        $('.settings-icon').click(function(){
+            $('.settings-menu').toggleClass('hide');
+        })
+
+        $('.settings-picture-block img').click(function(){
+            $(this).toggleClass('full-size');
+        })
+
+        $('[name=back-img]').change(function(){
+            var val=$(this)
+                .closest('.settings-picture-block')
+                .find('img')
+                .attr('src');
+            $('body').css({"background":"url("+val+")","background-position":"center","background-size":"cover"});
+        });
+
 })
