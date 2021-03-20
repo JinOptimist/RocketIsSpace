7d06a64481f11d86a4232428939337131ce2872c
alesyalis <alesya.lis.1@mail.ru>
2021-03-20 22:43:39 +0300

    n
    nh

---
+++ html/js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function (){
+    
+    $('.popup .cover').click(function(){
+        $('.popup').hide(2000);
+    });
+
+    $('.rocket .icon.close').click(function(){
+        var iconClose = $(this);//Тот по которому кликнули
+        iconClose
+            .closest('.rocket')
+            .hide();
+    });
+
+    $('.rocket img').click(function(){
+        //Добавляет класс full-size если его не было
+        //убирает класс full-size если он был
+        $(this).toggleClass('full-size');
+    });
+
+    $('.login').click(function(){
+        var userName = $('.user-login').val();
+        $('.header-text').text(userName);
+    });
+
+    $('.header-text').click(function(){
+        var headerText = $(this).text();
+        $('.user-login').val(headerText);
+        
+    });
+    $('.name').click(function (){
+        $(this)
+            .closest('.view')
+            .hide()
+            .addClass('.edit');
+            
+    });
