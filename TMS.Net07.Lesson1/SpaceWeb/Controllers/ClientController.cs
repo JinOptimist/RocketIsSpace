34292f6a292d92158a943863c7f27a9b612abfbc
alesyalis <alesya.lis.1@mail.ru>
2021-03-22 21:36:17 +0300

    web

---
+++ TMS.Net07.Lesson1/SpaceWeb/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaceWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceWeb.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Client()
+        {
+            var input = new InputViewModel()
+            {
+                Name = "Alesya"
+
+            };
+            return View(input);
+        }
+    }
+}
