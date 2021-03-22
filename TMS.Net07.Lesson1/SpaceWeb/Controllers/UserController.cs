18d66ae3be7a7f0f861148de47b8e191393ef66e
JinOptimist <88jin@mail.ru>
2021-03-22 19:36:17 +0300

    Add webProject

---
+++ TMS.Net07.Lesson1/SpaceWeb/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaceWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Profile()
+        {
+            var model = new ProfileViewModel()
+            {
+                Name = "Иван",
+                DateRegistration = new DateTime(2000, 1, 10)
+            };
+            return View(model);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
