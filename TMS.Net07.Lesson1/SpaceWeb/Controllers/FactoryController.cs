f4f8286c011e8d64d4988dfafe7aa22d7ff206ff
Vlad <vlad.eroshenko.96@mail.ru>
2021-03-23 17:02:38 +0300

    add Factory mainPage

---
+++ TMS.Net07.Lesson1/SpaceWeb/Controllers/FactoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaceWeb.Controllers
+{
+    public class FactoryController : Controller
+    {
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
