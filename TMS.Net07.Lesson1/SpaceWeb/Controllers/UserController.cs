0436aa9c1ca49064ab28a53f92c172697e7485c4
JinOptimist <88jin@mail.ru>
2021-03-24 20:48:07 +0300

    Bad login and register

--- TMS.Net07.Lesson1/SpaceWeb/Controllers/UserController.cs
+++ TMS.Net07.Lesson1/SpaceWeb/Controllers/UserController.cs
@@ -9,6 +9,12 @@ namespace SpaceWeb.Controllers
 {
     public class UserController : Controller
     {
+        //Это плохо. Удалить как только добавим БД
+        public static List<ProfileViewModel> Users 
+            = new List<ProfileViewModel>();
+
+        public static int Counter = 0;
+
         public IActionResult Profile()
         {
             var model = new List<RocketPreviewViewModel>();
@@ -36,20 +42,80 @@ namespace SpaceWeb.Controllers
         [HttpGet]
         public IActionResult Login()
         {
-            var model = new ProfileViewModel();
+            var model = new RegistrationViewModel();
             return View(model);
         }
 
         [HttpPost]
-        public IActionResult Login(ProfileViewModel model)
+        public IActionResult Login(RegistrationViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
 
-            model.Bio = model.UserName + model.Password;
+            var user = Users
+                .SingleOrDefault(x => x.UserName == model.Login);
+
+            if (user == null)
+            {
+                ModelState.AddModelError(
+                    nameof(RegistrationViewModel.Login),
+                    "Нет такого пользователя");
+                return View(model);
+            }
+            
+            if (user.Password != model.Password)
+            {
+                ModelState.AddModelError(
+                    nameof(RegistrationViewModel.Password), 
+                    "Не правильный праоль");
+                return View(model);
+            }
+
+            return RedirectToAction("Profile", "User");
+        }
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            var model = new RegistrationViewModel();
             return View(model);
         }
+
+        [HttpPost]
+        public IActionResult Registration(RegistrationViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //Старый способ.
+            //var isUserUniq = true;
+            //foreach (var user in Users)
+            //{
+            //    if (user.UserName == model.Login)
+            //    {
+            //        isUserUniq = false;
+            //    }
+            //}
+
+            //Новый способ LINQ
+            var isUserUniq = 
+                Users.All(user => user.UserName != model.Login);
+            if (isUserUniq)
+            {
+                Users.Add(new ProfileViewModel()
+                {
+                    DateRegistration = DateTime.Now,
+                    UserName = model.Login,
+                    Password = model.Password
+                });
+            }
+
+            return View(model);
+        }
+
     }
 }
