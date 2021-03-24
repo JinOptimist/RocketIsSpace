f0ad95dfacfe61ac3b9998a9fdddb5176f1129bd
Vlad <vlad.eroshenko.96@mail.ru>
2021-03-24 22:04:35 +0300

    sad

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/comfort.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/comfort.js
@@ -1,11 +1,20 @@
 $(document).ready(function (){
+	// $('.log').click(function(){
+	// 	$(this).hide();
+	// 	$('.login-block').hide();
+	// })
+	//
+	// $('.log-in').click(function(){
+	// 	$('.log').show();
+	// 	$('.login-block').show();
+	// })
 	$('.log').click(function(){
-		$(this).hide();
-		$('.login-block').hide();
+		$(this).addClass('hide');
+		$('.login-block').addClass('hide');
 	})
 
 	$('.log-in').click(function(){
-		$('.log').show();
-		$('.login-block').show();
+		$('.log').removeClass('hide');
+		$('.login-block').removeClass('hide');
 	})
 })
