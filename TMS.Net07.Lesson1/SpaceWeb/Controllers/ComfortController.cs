8a20981550090e711570cfc551d43f3f07d84bc7
Yana <77960186+yanakrupets@users.noreply.github.com>
2021-03-25 23:29:35 +0300

    +menyLayout

---
+++ TMS.Net07.Lesson1/SpaceWeb/Controllers/ComfortController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceWeb.Controllers
+{
+    public class ComfortController : Controller
+    {
+        public IActionResult ComfortPage()
+        {
+            return View();
+        }
+    }
+}
