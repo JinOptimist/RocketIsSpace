9f13f6b3ea8d81c5c969caa303ee7255e8484cdb
alesyalis <alesya.lis.1@mail.ru>
2021-03-25 23:33:29 +0300

    addClientController and view(Home, Contacts)

---
+++ TMS.Net07.Lesson1/SpaceWeb/Controllers/ClientController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaceWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpaceWeb.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Client()
+        {
+            var input = new RegistrationViewModel();
+           
+            return View(input);
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var model = new ProfileViewModel();
+            return View(model);
+        }
+        
+        [HttpPost]
+        public IActionResult Login(ProfileViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            model.Bio = model.UserName + model.Password;
+            return View(model);
+        }
+        public IActionResult Home()
+        {
+            var model = new RocketPreviewViewModel()
+            {
+                Name = "Soyus",
+                Url = "image/Client/start.jpg"
+            };
+
+            return View(model);
+        }
+        public IActionResult Contacts()
+        {
+            var input = new ContactsVieModel()
+            {
+                PhoneNumber = "+375291191293",
+                Email = "alesya.lis.1@mail.ru",
+                PostAddress = "Belarus, Minsk, Timeriazeva 67"
+            };
+            return View(input);
+        }
+    }
+}
