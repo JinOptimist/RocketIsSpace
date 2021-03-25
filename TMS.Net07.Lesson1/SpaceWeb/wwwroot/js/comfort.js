e5b5e3ecc3d372cc3632f06c524109d8e251a022
Yana <77960186+yanakrupets@users.noreply.github.com>
2021-03-25 21:02:38 +0300

    a

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/comfort.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/comfort.js
@@ -1,11 +1,11 @@
 $(document).ready(function (){
-	$('.log').click(function(){
-		$(this).hide();
-		$('.login-block').hide();
+	$('.log').click(function () {
+		$(this).addClass('hide');
+		$('.login-block').addClass('hide');
 	})
 
-	$('.log-in').click(function(){
-		$('.log').show();
-		$('.login-block').show();
+	$('.log-in').click(function () {
+		$('.log').removeClass('hide');
+		$('.login-block').removeClass('hide');
 	})
 })
