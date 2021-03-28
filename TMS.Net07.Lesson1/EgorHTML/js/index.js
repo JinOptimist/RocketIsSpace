1465405b090f68816240e5965fd8ced7b754842a
Egor <egorfedorenko.w@gmail.com>
2021-03-28 16:32:11 +0300

    Added js, made animations for icons

---
+++ TMS.Net07.Lesson1/EgorHTML/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    
+$('.circle').hover(function(){
+    $(this).children()
+        .toggleClass('material-icons-outlined')
+        .toggleClass('material-icons');
+});
+
+});
