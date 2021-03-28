610914b9b2875fbc5e9b4a2cccccdcdb37ceb977
Geek-Dmitry <salata.dzmitriy90@gmail.com>
2021-03-28 18:56:18 +0300

    feat mechanical page added to main project
    -link in factory added
    
    -css for mechanical page added
    
    images for mechanical pa added

---
+++ TMS.Net07.Lesson1/SpaceWeb/Controllers/MechanicController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceWeb.Controllers
+{
+    public class MechanicController : Controller
+    {
+        public IActionResult MechanicPage()
+        {
+            return View();
+        }
+    }
+}
