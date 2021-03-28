848e6766064369ab837de5f7f53c99240e2021ea
Vlad <vlad.eroshenko.96@mail.ru>
2021-03-28 19:53:58 +0300

    add Registration

---
+++ TMS.Net07.Lesson1/SpaceWeb/Models/RocketProfileViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using HumansResources.Humans.Persons;
+
+namespace SpaceWeb.Models
+{
+    public class RocketProfileViewModel
+    {
+        public RocketProfileViewModel(FactoryRegistrationViewModel model)
+        {
+            Person = new Person(null, null, model.Email)
+            {
+                Name = model.Name, Surname = model.LastName, BirthDate = model.DateOfBirth
+            };
+            UserName = model.UserName;
+            Password = model.Password;
+        }
+        public Person Person { get; set; }
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
