799dda7cd504d10bc8ce3562bc3000ec7e0af934
Egor <egorfedorenko.w@gmail.com>
2021-04-10 13:07:53 +0300

    Revert "Made standalone ComfortController"
    This reverts commit b03526b2ee7ad11d1b2c85e0bcf691e2c24a8918.

--- TMS.Net07.Lesson1/SpaceWeb/Controllers/RocketController.cs
+++ TMS.Net07.Lesson1/SpaceWeb/Controllers/RocketController.cs
@@ -8,11 +8,40 @@ namespace SpaceWeb.Controllers
     public class RocketController : Controller
     {
         private RocketProfileRepository _rocketProfileRepository;
-        public RocketController(RocketProfileRepository rocketProfileRepository)
+        private ComfortRepository _comfortRepository;
+        public RocketController(RocketProfileRepository rocketProfileRepository,ComfortRepository comfortRepository)
         {
             _rocketProfileRepository = rocketProfileRepository;
+            _comfortRepository = comfortRepository;
         }
-        
+
+        [HttpGet]
+        public IActionResult ComfortPage()
+        {
+            var model = new ComfortFormViewModel();
+            //return View(model);
+            return View("Comfort/ComfortPage", model);
+        }
+
+        [HttpPost]
+        public IActionResult ComfortPage(ComfortFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Comfort/ComfortPage",viewModel);
+            }
+
+            var comfort = new Comfort()
+            {
+                ToiletCount = viewModel.ToiletCount,
+                KitchenSeatsCount = viewModel.KitchenSeatsCount,
+                StorageCapacity = viewModel.StorageCapacity,
+                SleepingCapsulesCount = viewModel.SleepingCapsulesCount
+            };
+
+            _comfortRepository.Save(comfort);
+            return RedirectToAction("ComfortPage");
+        }
         // [HttpGet]
         // public IActionResult Login()
         // {
@@ -92,6 +121,26 @@ namespace SpaceWeb.Controllers
         //     var answer = RocketUsers.Any(x => x.UserName == name);
         //     return Json(answer);
         // }
+
+        public IActionResult ToiletPage()
+        {
+            return View("Comfort/ToiletPage");
+        }
+
+        public IActionResult KitchenPage()
+        {
+            return View("Comfort/KitchenPage");
+        }
+
+        public IActionResult CCenterPage()
+        {
+            return View("Comfort/CCenterPage");
+        }
+
+        public IActionResult CapsulePage()
+        {
+            return View("Comfort/CapsulePage");
+        }
 
         public IActionResult Rocket()
         {
