7429f0a5e9daf3a203af58e3ee5258866846fb8a
JOKER <34417709+Alexy-Def@users.noreply.github.com>
2021-04-20 08:49:06 +0300

    Revert "Revert "Merge branch 'master' into Team1Bank.Alesya""
    This reverts commit e5dc16b2e3c48490d2da8c7a2cd6df8607a817ee.

---
+++ TMS.Net07.Lesson1/SpaceWeb/Migrations/20210419161211_AddJobType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using SpaceWeb.EfStuff.Model;
+
+namespace SpaceWeb.Migrations
+{
+    public partial class AddJobType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "JobType",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: JobType.Junior);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobType",
+                table: "Users");
+        }
+    }
+}
