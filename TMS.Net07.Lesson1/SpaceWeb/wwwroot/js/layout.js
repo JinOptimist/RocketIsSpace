5f19151e14c8ad03d70d4c58e931ed5104dc49b1
JinOptimist <88jin@mail.ru>
2021-04-21 21:54:19 +0300

    Fix rocket layout

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/layout.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/layout.js
@@ -1,11 +1,9 @@
 ﻿$(document).ready(function () {
-	$('.log').click(function () {
-		$(this).addClass('hide');
-		$('.login-block').addClass('hide');
+	$('.popup-cover').click(function () {
+		$('.nice-popup').addClass('hide');
 	});
 
 	$('.log-in').click(function () {
-		$('.log').removeClass('hide');
-		$('.login-block').removeClass('hide');
+		$('.nice-popup').removeClass('hide');
 	});
 });
