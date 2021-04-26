4868eeb24b873e2b3bcf265bfbbcee8f67cf8669
JinOptimist <88jin@mail.ru>
2021-04-26 20:16:43 +0300

    Add UT for SpaceWeb

---
+++ TMS.Net07.Lesson1/SpaceWeb/EfStuff/Repositories/IRepository/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using SpaceWeb.EfStuff.Model;
+using System.Collections.Generic;
+
+namespace SpaceWeb.EfStuff.Repositories
+{
+    public interface IBaseRepository<ModelType> where ModelType : BaseModel
+    {
+        ModelType Get(long id);
+        List<ModelType> GetAll();
+        void Remove(long id);
+        void Remove(ModelType model);
+        void Save(ModelType model);
+    }
+}
