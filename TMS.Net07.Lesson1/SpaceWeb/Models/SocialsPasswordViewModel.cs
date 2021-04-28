9d044795cedc9bda53544e5fda26a80ce0a244a5
JOKER <sasha.golubovich@tut.by>
2021-04-28 20:30:08 +0300

    Edit a lot of things (edit link on main footer, add SocialPasswords

---
+++ TMS.Net07.Lesson1/SpaceWeb/Models/SocialsPasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceWeb.Models
+{
+    public class SocialsPasswordViewModel
+    {
+        [Required]
+        public SocialsPassword Password { get; set; }
+
+        public string Link { get; set; }
+    }
+}
--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/layout.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/layout.js
@@ -6,4 +6,20 @@
 	$('.log-in').click(function () {
 		$('.nice-popup').removeClass('hide');
 	});
+
+	$('.socials-popup-cover').click(function () {
+		$('.socials-popup').addClass('hide');
+	});
+
+	$('.footer-icon.telegram.tmsgroup').click(function () {
+		$('.socials-popup').removeClass('hide');
+		$('.socials-block.youtube').addClass('hide');
+		$('.socials-block.telegram').removeClass('hide');
+	});
+
+	$('.footer-icon.youtube.teacher').click(function () {
+		$('.socials-popup').removeClass('hide');
+		$('.socials-block.telegram').addClass('hide');
+		$('.socials-block.youtube').removeClass('hide');
+	});
 });
