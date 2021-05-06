7de932643a2ca2c00289fedd5915a585c80953d1
KiriIl <375447360162@mail.ru>
2021-05-06 17:01:24 +0300

    refactor: css human style

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/human/human.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/human/human.js
@@ -1,1 +1,6 @@
-﻿
+﻿$(document).ready(function () {
+    $('.human-department-clickable').click(function () {
+        var currentDepartment = $(this).parent('label').find('input');
+        console.log('s');
+    });
+});
