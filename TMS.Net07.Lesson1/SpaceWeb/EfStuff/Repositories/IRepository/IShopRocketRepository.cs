7d9622a7bc90e93801e0399e321edcaead6a9c53
Vlad <vlad.eroshenko.96@mail.ru>
2021-05-23 22:50:34 +0300

    add interfaces

---
+++ TMS.Net07.Lesson1/SpaceWeb/EfStuff/Repositories/IRepository/IShopRocketRepository.cs
@@ -0,0 +1,8 @@
+using SpaceWeb.EfStuff.Model;
+
+namespace SpaceWeb.EfStuff.Repositories.IRepository
+{
+    public interface IShopRocketRepository:IBaseRepository<Rocket>
+    {
+    }
+}
--- TMS.Net07.Lesson1/SpaceWeb/EfStuff/Repositories/ShopRocketRepository.cs
+++ TMS.Net07.Lesson1/SpaceWeb/EfStuff/Repositories/ShopRocketRepository.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using SpaceWeb.EfStuff.Model;
+using SpaceWeb.EfStuff.Repositories.IRepository;
 
 namespace SpaceWeb.EfStuff.Repositories
 {
-    public class ShopRocketRepository: BaseRepository<Rocket>
+    public class ShopRocketRepository: BaseRepository<Rocket>, IShopRocketRepository
     {
         public ShopRocketRepository(SpaceDbContext spaceDbContext) : base(spaceDbContext)
         {
