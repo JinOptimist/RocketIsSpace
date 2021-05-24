b64669e685e6d896a9f2ac9ab2caa1d452199da2
Yana <77960186+yanakrupets@users.noreply.github.com>
2021-05-24 20:03:14 +0300

    ut bankaccount

--- TMS.Net07.Lesson1/SpaceWeb.Test/Presentation/BankPresentationTest.cs
+++ TMS.Net07.Lesson1/SpaceWeb.Test/Presentation/BankPresentationTest.cs
@@ -16,36 +16,66 @@ namespace SpaceWeb.Test.Presentation
         private Mock<IProfileRepository> _mockRepository;
         private Mock<IMapper> _mockMapper;
 
-        [Test]
-        [TestCase(1)]
-        [TestCase(0)]
-        public void GetProfileViewModel(long id)
+        [SetUp]
+        public void Setup()
         {
             _mockRepository = new Mock<IProfileRepository>();
             _mockMapper = new Mock<IMapper>();
             _bankPresentation = new BankPresentation(
                 _mockRepository.Object,
                 _mockMapper.Object);
+        }
 
-            var userprofile = new SpaceWeb.EfStuff.Model.Profile() {
+        [Test]
+        [TestCase(1)]
+        public void GetProfileViewModel_1(long id)
+        {
+            var userprofile = new SpaceWeb.EfStuff.Model.Profile()
+            {
                 Id = id,
                 Name = "A",
                 SurName = "B"
             };
+            var prof = new UserProfileViewModel()
+            {
+                Id = id,
+                Name = "A",
+                SurName = "B"
+            };
+
             _mockRepository.Setup(x => x.Get(id)).Returns(userprofile);
+            _mockMapper.Setup(x => x.Map<UserProfileViewModel>(userprofile)).Returns(prof);
 
             var profile = _bankPresentation.GetProfileViewModel(id);
 
-            _mockMapper.Verify(x => x.Map<UserProfileViewModel>(userprofile));
+            _mockMapper.Verify(x => x.Map<UserProfileViewModel>(userprofile), Times.AtLeastOnce);
 
-            if (id > 0)
+            Assert.AreEqual(profile.Name, prof.Name);
+        }
+
+        [Test]
+        [TestCase(0)]
+        public void GetProfileViewModel_0(long id)
+        {
+            var userprofile = new SpaceWeb.EfStuff.Model.Profile()
             {
-                Assert.AreEqual(profile.Name, userprofile.Name);
-            }
-            else
+                Id = id,
+                Name = "A",
+                SurName = "B"
+            };
+            var prof = new UserProfileViewModel()
             {
-                Assert.AreEqual(profile.Name, null);
-            }
+                Id = id
+            };
+
+            _mockRepository.Setup(x => x.Get(id)).Returns(userprofile);
+            _mockMapper.Setup(x => x.Map<UserProfileViewModel>(userprofile)).Returns(prof);
+
+            var profile = _bankPresentation.GetProfileViewModel(id);
+
+            _mockMapper.Verify(x => x.Map<UserProfileViewModel>(userprofile), Times.AtLeastOnce);
+
+            Assert.AreEqual(profile.Name, null);
         }
     }
 }
