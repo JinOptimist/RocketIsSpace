6ef17fa4b7fc50ff122245f367fa1be77acf9e07
Yana <77960186+yanakrupets@users.noreply.github.com>
2021-06-05 18:44:32 +0300

    almost ready

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/Rocket/additionPage.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/Rocket/additionPage.js
@@ -1,8 +1,45 @@
 $(document).ready(function () {
 
+    var time = 300;
+
     $('.circle').hover(function () {
         $(this).children()
             .toggleClass('material-icons-outlined')
             .toggleClass('material-icons');
     });
+
+
+    $('.circle').mouseenter(function () {
+        if (isAnimationActive) {
+            return;
+        }
+        isAnimationActive = true
+
+        $(this).animate({
+            width: 130 * 2,
+            height: 130 * 2
+        }, time)
+        $(this).removeClass('not-chosen')
+        $('.not-chosen').animate({
+            width: 130 / 2,
+            height: 130 / 2
+        }, time)
+
+    })
+
+
+    $('.circle').mouseleave(function () {
+        $(this).animate({
+            width: 130,
+            height: 130
+        }, time)
+        $('.not-chosen').animate({
+            width: 130,
+            height: 130
+        }, time)
+        $(this).addClass('not-chosen')
+
+        isAnimationActive = false
+    })
+
 });
