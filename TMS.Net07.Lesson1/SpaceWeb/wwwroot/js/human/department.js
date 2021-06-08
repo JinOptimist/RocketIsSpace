f15b5281397f8c88b03a14ae55ea6a012c900bdd
KiriIl <375447360162@mail.ru>
2021-06-08 17:18:05 +0300

    add flip animation

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/human/department.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/human/department.js
@@ -1,14 +1,24 @@
 ﻿$(document).ready(function () {
-    var tag = '.employes-list .human-department-info';
+
+    function setter() {
+        return {
+            trigger: 'click',
+            speed: 1000
+        }
+    };
+
+    var tag = '.employes-list .human-department-employee';
+
     $('.human-department-clickable .department-info-block').click(function () {
         var departmentId = $(this).parent('label').find('input').val();
         var url = '/Human/UpdateEmployes?idDepartment=' + departmentId;
-        $('.employes-list .human-department-info:not(.clone)').remove();
+        $('.employes-list .human-department-employee:not(.clone)').remove();
         $.getJSON(url)
             .done(function (employes) {
                 for (var i = 0; i < employes.length; i++) {
-                    var block = $('.clone').clone(true).appendTo('.employes-list');
+                    var block = $('.clone').clone().appendTo('.employes-list');
                     block.removeClass('clone undisplayable');
+                    block.flip(setter());
                     InsertText(block, '#name', employes[i].name);
                     InsertText(block, '#surname', employes[i].surname);
                     InsertText(block, '#specification', employes[i].position);
@@ -19,4 +29,7 @@
     function InsertText(block, divToFind, arg) {
         block.find(divToFind).append(arg);
     };
+
+    $('.flip-flop').flip(setter());
+
 });
