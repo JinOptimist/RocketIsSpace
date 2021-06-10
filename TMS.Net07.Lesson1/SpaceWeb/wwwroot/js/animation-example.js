fc37eb2de7d4f8c03952384bb10c34cfcef23bf6
Yana <77960186+yanakrupets@users.noreply.github.com>
2021-06-10 15:38:26 +0300

    fpt

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/animation-example.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/animation-example.js
@@ -1,9 +1,19 @@
 ﻿$(document).ready(function () {
     var images = [
-        '/image/bank/card-shopp.jpg',
-        '/image/bank/card-mocn.jpg',
-        '/image/bank/card-x.jpg'
+        '/image/carousel/ph1.jpg',
+        '/image/carousel/ph2.jpg',
+        '/image/carousel/ph3.jpg',
+        '/image/carousel/ph4.jpg',
+        '/image/carousel/ph5.jpg',
+        '/image/carousel/ph6.jpg'
     ];
+
+    var val = 1;
+
+    $('.dot').click(function () {
+        $('.dot').removeClass('active');
+        $(this).addClass('active');
+    })
 
     carouselModule.initialize(
         '.animate-block',
