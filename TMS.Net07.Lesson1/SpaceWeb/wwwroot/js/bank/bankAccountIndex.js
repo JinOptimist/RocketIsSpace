7c154cbde8f2e1972ec83f50a7c3c7d2ebcccedc
Valentin Karankevich <v.v.karankevich@gmail.com>
2021-06-17 12:09:52 +0300

    account new windows

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/bank/bankAccountIndex.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/bank/bankAccountIndex.js
@@ -4,28 +4,73 @@ $(document).ready(function () {
 
     var time = 1 * 1000;
 
-    $('.button.deposit').click(function () {
+
+    $('.content-box .button-list .button.show-menu').click(function () {
+
+        var operation = $(this).attr('class').replace('button ', '').replace(' show-menu', '');
+
+        console.log(operation);
 
         $('.button-list').toggleClass('hide');
 
-        $('.deposit-form.container').toggleClass('hide');
+        $(`.${operation}-form.container`).toggleClass('hide');
     })
 
-    $('.cancel-deposit.button').click(function (env) {
-
-        $('.deposit-form.container').toggleClass('hide');
-
+    $('.container .form .buttons .cancel').click(function () {
+        $(this).closest('.container').toggleClass('hide');
         $('.button-list').toggleClass('hide');
-
-        env.preventDefault();
     })
 
-    $('.make-deposit.button').click(function (env) {
 
-        $('.deposit-form.container').toggleClass('hide');
+    //$('.button.deposit').click(function () {
 
-        $('.button-list').toggleClass('hide');
+    //    $('.button-list').toggleClass('hide');
 
-        env.preventDefault();
-    })
+    //    $('.deposit-form.container').toggleClass('hide');
+    //})
+
+    //$('.cancel-deposit.button').click(function (env) {
+
+    //    $('.deposit-form.container').toggleClass('hide');
+
+    //    $('.button-list').toggleClass('hide');
+
+    //    env.preventDefault();
+    //})
+
+    //$('.make-deposit.button').click(function (env) {
+
+    //    $('.deposit-form.container').toggleClass('hide');
+
+    //    $('.button-list').toggleClass('hide');
+
+    //    env.preventDefault();
+    //})
+
+
+    //$('.button.withdrawal').click(function () {
+
+    //    $('.button-list').toggleClass('hide');
+
+    //    $('.withdrawal-form.container').toggleClass('hide');
+    //})
+
+    //$('.cancel-withdrawal.button').click(function (env) {
+
+    //    $('.withdrawal-form.container').toggleClass('hide');
+
+    //    $('.button-list').toggleClass('hide');
+
+    //    env.preventDefault();
+    //})
+
+    //$('.make-withdrawal.button').click(function (env) {
+
+    //    $('.withdrawal-form.container').toggleClass('hide');
+
+    //    $('.button-list').toggleClass('hide');
+
+    //    env.preventDefault();
+    //})
+
 });
