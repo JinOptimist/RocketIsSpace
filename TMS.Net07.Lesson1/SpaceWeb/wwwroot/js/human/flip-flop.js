dd3199aeba0af2dc6bf8e16a1e60d4f81fefb492
sined07 <sined07@yandex.ru>
2021-06-21 16:30:16 +0300

    feat: draft flip-flop

---
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/human/flip-flop.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    var element = $('.flip-flop');
+    element.on('click', () => {
+        flip(element);
+    });
+})
+
+
+function flip(element) {
+    var foreground = element.find('.foreground');
+    var background = element.find('.background');
+    var from = getScaleX(element);
+    var to = foreground.hasClass('hide') ? 1 : -1;
+
+    $({ deg: from }).animate({ deg: to }, {
+        duration: 500,
+        step: function (now) {
+            var prev = getScaleX(element);
+            if ((prev >= 0 && now <= 0) || (prev <= 0 && now >= 0)) {
+                foreground.toggleClass('hide');
+                background.toggleClass('show');
+            }
+            element.css({
+                transform: 'scaleX(' + now + ')'
+            });
+        }
+    });
+
+    function getScaleX(element) {
+        return element.css('transform').replace(/\(|\)|matrix/g, '').split(',')[0];
+    }
+}
