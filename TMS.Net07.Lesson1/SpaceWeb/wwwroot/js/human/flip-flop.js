b5d4c42f7f96bdce63107bfd6c944076b1f39b6e
sined07 <sined07@yandex.ru>
2021-06-22 16:15:29 +0300

    refactor: flip-flop

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/human/flip-flop.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/human/flip-flop.js
@@ -1,29 +1,14 @@
-﻿$(document).ready(function () {
-
-    $('#flip-flop').each(function (element) {        
-        console.log(element);
-        element.on('click', () => flip(element));        
-    });
-
-
-    //var element = $('.flip-flop');
-    //element.on('click', () => {
-    //    flip(element);
-    //});
-})
-
-
-function flip(element) {
+﻿function flip(element) {
     var foreground = element.find('.foreground');
     var background = element.find('.background');
     var from = getScaleX(element);
     var to = foreground.hasClass('hide') ? 1 : -1;
 
-    $({ deg: from }).animate({ deg: to }, {
+    $({ scale: from }).animate({ scale: to }, {
         duration: 500,
         step: function (now) {
-            var prev = getScaleX(element);
-            if ((prev >= 0 && now <= 0) || (prev <= 0 && now >= 0)) {
+            var current = getScaleX(element);
+            if ((current >= 0 && now <= 0) || (current <= 0 && now >= 0)) {
                 foreground.toggleClass('hide');
                 background.toggleClass('show');
             }
@@ -34,7 +19,6 @@ function flip(element) {
     });
 
     function getScaleX(element) {
-        console.log(element);
         return element.css('transform').replace(/\(|\)|matrix/g, '').split(',')[0];
     }
 }
