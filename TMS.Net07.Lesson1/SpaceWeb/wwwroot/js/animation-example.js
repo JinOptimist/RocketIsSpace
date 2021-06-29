84339a8d482e8c9ce543ca57366d046f970c88dd
Yana <77960186+yanakrupets@users.noreply.github.com>
2021-06-29 16:33:43 +0300

    dots

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/animation-example.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/animation-example.js
@@ -1,22 +1,22 @@
 ﻿$(document).ready(function () {
+    var images = [];
     $.get('/Home/ImageForCarousel')
         .done(function (answer) {
-            var images = [];
             for (i = 0; i < answer.length; i++) {
                 var startIndex = answer[i].indexOf('\\image\\carousel\\');
                 images[i] = answer[i].slice(startIndex).replace(/\\/g, '/');
             }
+            carouselModule.initialize(
+                '.animate-block',
+                '.animate-block-dots',
+                images,
+                {
+                    time: 2000
+                });
         })
         .fail(function () {
 
         });
 
     
-
-    carouselModule.initialize(
-        '.animate-block',
-        images,
-        {
-            time: 2000
-        });
 });
