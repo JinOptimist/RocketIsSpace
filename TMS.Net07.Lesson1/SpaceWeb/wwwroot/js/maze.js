eeef2714a4785bf53392cb367e4be8f21b36e0eb
KiriIl <375447360162@mail.ru>
2021-07-21 15:39:24 +0300

    add module maze-way.js

--- TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/maze.js
+++ TMS.Net07.Lesson1/SpaceWeb/wwwroot/js/maze.js
@@ -1,53 +1,9 @@
 ﻿$(document).ready(function () {
 
-    var drawedVertexes = [];
-    var wayIndexer;
-    var currentInterval;
-    var time = 1000;
-    var color = 'black'
-
-    $('.maze-way').click(function () {
-        var x = $(this).find('.cell').attr('x');
-        var y = $(this).find('.cell').attr('y');
-        $.get(`/Home/PossibleWays?x=${x}&y=${y}`)
-            .done(function (data) {
-
-                SetDefault();
-                clearInterval(currentInterval);
-                currentInterval = setInterval(function () { testFunc(data) }, time);
-                
-            });
-    });
-
-    function testFunc(data) {
-
-        SetDefaultVertexes();
-
-        for (var j = 0; j < data[wayIndexer].cells.length; j++) {
-            var pointX = data[wayIndexer].cells[j].x;
-            var pointY = data[wayIndexer].cells[j].y;
-            var vertex = $(`.cell[x=${pointX}][y=${pointY}]`)
-            vertex.parent().css('background', color);
-            drawedVertexes.push(vertex);
-        }
-
-        wayIndexer++;
-        if (wayIndexer >= data.length) {
-            wayIndexer = 0;
-        }
-
-    }
-
-    function SetDefault() {
-        wayIndexer = 0;
-        SetDefaultVertexes();
-    }
-
-    function SetDefaultVertexes() {
-        for (var i = drawedVertexes.length - 1; i >= 0; i--) {
-            drawedVertexes[i].parent().css('background', '');
-            drawedVertexes.pop();
-        }
-    }
+    var array = [
+        "road",
+        "gold"
+    ];
+    wayModule.initialize("maze-way", array, { color: "red" });
 
 });
